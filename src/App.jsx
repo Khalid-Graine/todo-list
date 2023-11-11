2314e2dc86f35427e51b1618b4f74d3403a9fde5
Khalid Graine <khalidgraine1@gmail.com>
2023-11-11 16:56:54 +0100

    Create mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Tasks, setTask] = useState([
+    {
+      id: 1,
+      title: "r r r r r r ",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "r r r 44444r r r ",
+      isDone: false,
+    },
+    {
+      id: 3,
+      title: "r r r r r r ",
+      isDone: false,
+    },
+  ]);
 
   return (
     <>
       <div className=" flex justify-center py-5">
-       <div className="md:w-6/12 w-full p-2  bg-red-100">
-        <form action="">
-          <input type="text" value={'write any things'} />
-          <button type="submit">add</button>
-        </form>
-        <ul>
-          <li>
-            <div>buy a coffee</div>
-            <div>
-            <button>done</button>
-            <button>delete</button>
-            </div>
-          </li>
-        </ul>
-       </div>
+        <div className="md:w-6/12 w-full p-2  bg-red-100">
+          <form action="">
+            <input type="text" value={"write any things"} />
+            <button type="submit">add</button>
+          </form>
+          <ul>
+            {Tasks.map((task) => (
+              <li key={task.id}>
+                <div>{task.title}</div>
+                <div className="flex gap-3">
+                  <button>done</button> 
+                  <button>delete</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
